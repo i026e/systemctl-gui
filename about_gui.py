3aeae37d3915630ff7bc9e4cbc49fc14231d38f8
i026e <klev.paul@gmail.com>
2017-01-08 17:40:07 +0300

    about

--- about_gui.py
+++ about_gui.py
@@ -6,7 +6,7 @@ Created on Mon Dec 26 10:46:52 2016
 @author: pavel
 """
 import os
-import sys 
+import sys
 import systemctl_commands as stl
 
 from gi.repository import Gtk
@@ -19,37 +19,37 @@ class AboutDialog(metaclass=Singleton):
         self.dialog = self.builder.get_object("about_window")
         self.dialog.connect("delete-event", self._on_hide)
         self.dialog.set_transient_for(parent_window)
-        
-        ok_button = self.builder.get_object("about_ok_button")
-        ok_button.connect("clicked", self._on_hide)
-        
+
         self.__init_view()
-        
+
     def __init_view(self):
         self.view =  self.builder.get_object("about_view")
-        template="OS: {os_ver} \n\n Python: {python_ver} \n\n Gtk: {gtk_ver} \n\n systemctl: {stl_ver}"        
-        
+        template="OS: {os_ver} \n\n Python: {python_ver} \n\n Gtk: {gtk_ver} \n\n systemctl: {stl_ver}"
+
         python_ver = sys.version
-        
+
         uname = os.uname()
         os_ver = "{sysname} {machine} {release} {version} {nodename}".format(\
         nodename = uname.nodename, sysname = uname.sysname,\
         machine = uname.machine, release=uname.release, version=uname.version)
-        
+
         gtk_ver = "{major}.{minor}.{micro}".format(\
         major=Gtk.get_major_version(), minor= Gtk.get_minor_version(),\
         micro = Gtk.get_micro_version())
-        
+
         stl_ver = stl.get_systemctl_version()
-        
-        text = template.format(os_ver=os_ver, python_ver=python_ver, 
+
+        text = template.format(os_ver=os_ver, python_ver=python_ver,
                                stl_ver=stl_ver, gtk_ver=gtk_ver)
-        self.view.get_buffer().set_text(text)
-        
+
+
+        self.dialog.set_comments(text)
+
+
     def show(self):
-        self.dialog.run()
-        
-    def _on_hide(self, *args):        
+        response = self.dialog.run()
+        self._on_hide()
+
+    def _on_hide(self, *args):
         self.dialog.hide()
         return True
-        
